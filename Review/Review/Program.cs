4254b20128a0116b889d78139694eca06beff9ef
Kailee Orr <kailee@ou.edu>
2020-01-23 12:33:48 -0600

    Same with dictionary

--- Review/Review/Program.cs
+++ Review/Review/Program.cs
@@ -11,6 +11,7 @@ namespace Review
     {
         static void Main(string[] args)
         {
+            //parallel arrays = means each element at the same position is part of the same entity
             int[] studentIds = new int[3];
             string[] studentFirstNames = new string[3];
             string[] studentLastNames = { "Harris", "Ackerman", "Johnson" };
@@ -21,7 +22,39 @@ namespace Review
             studentFirstNames[1] = "Adam";
             studentFirstNames[2] = "Harry";
 
+            for (int i = 0; i < studentIds.Length; i++)
+            {
+                Console.WriteLine($"{studentLastNames[i]}, {studentFirstNames[i]} [{studentIds[i]}]");
+            }
 
+
+            //Dictionary - key/value pairs
+            Dictionary<int, string> students = new Dictionary<int, string>();
+
+            students.Add(1, "Katie Harris");
+            students.Add(2, "Adam Ackerman");
+            students.Add(3, "Harry Smith");
+
+            //if (students.ContainsKey(1))
+            //{
+            //  students[1] = "SOME NEW VALUE";
+            //
+            //}
+            // else
+            //{
+            //    students.Add(1, "SOME VALUE");
+            //}
+
+            foreach (var key in students.Keys)
+            {
+                Console.WriteLine($"{students[key]} [{key}].");
+            }
+          
+
+
+
+
+            Console.ReadKey();
 
 
 
