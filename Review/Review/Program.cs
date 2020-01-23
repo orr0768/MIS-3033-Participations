9bed8a067843f98edd637a894b6a4b93683f6b14
Kailee Orr <kailee@ou.edu>
2020-01-23 12:18:41 -0600

    created project and created arrays

---
+++ Review/Review/Program.cs
@@ -0,0 +1,30 @@
+﻿//Kailee Orr
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentIds = new int[3];
+            string[] studentFirstNames = new string[3];
+            string[] studentLastNames = { "Harris", "Ackerman", "Johnson" };
+            studentIds[0] = 1;
+            studentIds[1] = 2;
+            studentIds[2] = 3;
+            studentFirstNames[0] = "Katie";
+            studentFirstNames[1] = "Adam";
+            studentFirstNames[2] = "Harry";
+
+
+
+
+
+        }
+    }
+}
