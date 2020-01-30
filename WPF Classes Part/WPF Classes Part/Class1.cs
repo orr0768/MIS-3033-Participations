6bd2205176a127a5d596852a4b8f2b6d3c4fdcd8
Kailee Orr <kailee@ou.edu>
2020-01-30 13:00:47 -0600

    finished participation 1/30

---
+++ WPF Classes Part/WPF Classes Part/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Classes_Part
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+
+        public EntryForm()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+        }
+
+    
+        public override string ToString()
+        {
+            return Name + " " + Address + " " + ZipCode; 
+        }
+    }
+}
---
+++ WPF Classes Part/WPF Classes Part/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes_Part
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtName.Text = "";
+            txtAddress.Text = string.Empty;
+            txtZip.Text = string.Empty;
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            List<EntryForm> submissions = new List<EntryForm>();
+
+
+            EntryForm form1 = new EntryForm();
+            form1.Name = txtName.Text;
+            form1.Address = txtAddress.Text;
+            form1.ZipCode = Convert.ToInt32(txtZip.Text);
+
+            
+
+            lstKeepTrack.Items.Add(form1.ToString());
+
+            
+         
+
+            
+
+           
+
+        }
+    }
+}
