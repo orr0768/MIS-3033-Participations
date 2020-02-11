1a9e5f52f94e5f31c1beca2583f7e197cae21f8d
Kailee Orr <kailee@ou.edu>
2020-02-11 12:47:17 -0600

    created classes from rick and morty url

---
+++ Wpf_JSON_Rick_And_Morty/Wpf_JSON_Rick_And_Morty/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_JSON_Rick_And_Morty
+{
+    class RickAndMortyAPIResult
+    {
+
+        public InfoObject info { get; set; }
+
+        public List<ResultObject> results { get; set; }
+
+
+
+    }
+    public class ResultObject
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string gender { get; set; }
+        public string image { get; set; }
+    }
+
+    public class InfoObject
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+    }
+
+   
+}
