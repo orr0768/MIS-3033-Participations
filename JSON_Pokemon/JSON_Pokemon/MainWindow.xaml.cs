57eaffadbe1ae59c6e44d42ce34e5bae28820ead
Kailee Orr <kailee@ou.edu>
2020-02-13 13:18:03 -0600

    Created a WPF with Json and Pokemon

---
+++ JSON_Pokemon/JSON_Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            string path = "https://pokeapi.co/api/v2/pokemon";
+            AllPokemonAPI PokemonAPI;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(path).Result;
+
+                PokemonAPI = JsonConvert.DeserializeObject<AllPokemonAPI>(json);
+
+            }
+
+            foreach (var pokie in PokemonAPI.results)
+            {
+                lstResults.Items.Add(pokie);
+
+            }
+
+            
+
+
+        }
+
+
+
+        private void lstResults_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        {
+            var selectedCharacterfromlist = (AllPokemonResult)lstResults.SelectedItem;
+
+            PokieInfo PokieAPI;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(selectedCharacterfromlist.url).Result;
+
+                PokieAPI = JsonConvert.DeserializeObject<PokieInfo>(json);
+
+            }
+
+            foreach (var pokie in PokieAPI.front_default)
+            {
+                lstSprites.Items.Add(pokie);
+
+            }
+            
+        }
+    }
+}
---
+++ JSON_Pokemon/JSON_Pokemon/Pokemon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Pokemon
+{
+    class AllPokemonAPI
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public double previeous { get; set; }
+        public List<AllPokemonResult> results { get; set; }
+        public List<PokieInfo> sprites { get; set; }
+
+
+
+    }
+
+    public class PokieInfo
+
+    {
+        public double weight { get; set; }
+        public double height { get; set; }
+        public string front_default { get; set; }
+
+        public override string ToString()
+        {
+            return $"Weight: {weight}, Height: {height}";
+        }
+    }
+
+    public class AllPokemonResult
+
+    {
+        public string url { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    
+
+
+}
